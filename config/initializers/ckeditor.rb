e560d8b325706d54b8337c234363c8a4dbca2b4a
Michael Fashola <michaelfashola@gmail.com>
2017-03-17 20:45:42 -0400

    precompile ckeditor assets

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,4 +1,5 @@
 Ckeditor.setup do |config|
-  # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  config.cdn_url = "//cdn.ckeditor.com/4.6.1/basic/ckeditor.js"
+  config.assets_languages = ['en', 'fr']
+  config.assets_plugins = ['image', 'smiley']
+  config.authorize_with :cancan
 end
